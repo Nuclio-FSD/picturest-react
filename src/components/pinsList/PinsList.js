b4986d5dcdf2a63a393162bd73b7c9680029a541
Angel Blanco <angelbt91@gmail.com>
2021-07-06 23:15:44 +0200

    Add useForm handling + Refactor

--- src/components/pinsList/PinsList.js
+++ src/components/pinsList/PinsList.js
@@ -2,11 +2,32 @@ import { useEffect, useState } from "react";
 import PinCard from "../pinCard/PinCard";
 import "./PinsList.css";
 import UploadImageInput from "../uploadImageInput/UploadImageInput";
+import { useForm } from "react-hook-form";
 
 const PinsList = () => {
   const [pins, setPins] = useState([]);
   const [refresh, setRefresh] = useState(1);
-  const [pinName, setPinName] = useState();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // TODO add handling of sending to backend
+    // fetch("http://localhost:5001/api/pins", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(body),
+    // })
+    //   .then((response) => response.json())
+    //   .then((json) => setRefresh())
+    //   .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     fetch("http://localhost:5001/api/pins")
@@ -14,24 +35,6 @@ const PinsList = () => {
       .then((json) => setPins(json))
       .catch((err) => console.log(err));
   }, [refresh]);
-
-  const body = {
-    name: pinName,
-  };
-
-  const createPin = () => {
-    // TODO add handling of sending to Cloudinary
-    fetch("http://localhost:5001/api/pins", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((json) => setRefresh())
-      .catch((err) => console.log(err));
-  };
 
   return (
     <div>
@@ -41,10 +44,16 @@ const PinsList = () => {
           <PinCard pin={pin} key={pin.id} />
         ))}
       </div>
-      <form>
-        <input onChange={(event) => setPinName(event.target.value)}></input>
-        <UploadImageInput maxPhotos={4} />
-        <button onClick={() => createPin()}>Create</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <UploadImageInput
+          maxImages={4}
+          {...register("file", { required: true })}
+          updateInputValue={setValue}
+        />
+        <input className="pinsList__submit" type="submit" value="Upload" />
+        {errors.file && (
+          <span style={{ color: "red" }}>This field is required</span>
+        )}
       </form>
     </div>
   );
--- src/components/uploadImageInput/UploadImageInput.js
+++ src/components/uploadImageInput/UploadImageInput.js
@@ -1,18 +1,27 @@
 import "./UploadImageInput.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
-const UploadImageInput = ({ maxPhotos = 1 }) => {
-  // we use the ref to call click on the input when user clicks on the + sign
+const UploadImageInput = ({ maxImages = 1, name, updateInputValue }) => {
+  // if the user sets an incorrect number, set maxImages to 1
+  if (typeof maxImages !== "number" || maxImages < 1) maxImages = 1;
+
+  // use a ref on the input type=file element to call click on it when user clicks on the + sign
   const inputRef = useRef();
   const openInput = () => inputRef.current.click();
 
-  const filesSelectedRef = useRef([]); // to save the files selected between re-renders
-  const [photosToUpload, setPhotosToUpload] = useState([]); // to store the processed images
+  const filesSelectedRef = useRef([]); // to avoid losing the files selected between re-renders
+  const [imagesConverted, setImagesConverted] = useState([]); // to store the converted images
+  const [imagesUploaded, setImagesUploaded] = useState([]); // to store the uploaded images
+
+  // whenever there are new images uploaded to Cloudinary, we update the value to inform the parent
+  useEffect(() => {
+    updateInputValue(name, imagesUploaded);
+  }, [imagesUploaded]);
 
   const handleChange = () => {
-    // abort if we are trying to upload more photos than allowed
-    if (inputRef.current.files.length > maxPhotos - photosToUpload.length) {
-      alert(`Please, select up to ${maxPhotos} pictures.`);
+    // abort if we are trying to upload more images than allowed
+    if (inputRef.current.files.length > maxImages - imagesConverted.length) {
+      alert(`Please, select up to ${maxImages} pictures.`);
       inputRef.current.value = null;
       return;
     }
@@ -21,48 +30,94 @@ const UploadImageInput = ({ maxPhotos = 1 }) => {
     filesSelectedRef.current = [...inputRef.current.files];
     inputRef.current.value = null;
 
-    // for each file selected, we transform it to base64 and store it into the photosToUpload state variable
+    // for each file selected, we transform it to base64 and store it into the imagesConverted state variable
     filesSelectedRef.current.forEach((file) => {
       const reader = new FileReader();
 
       reader.onload = (event) => {
-        setPhotosToUpload((current) => [
-          ...current,
-          {
-            id: event.timeStamp,
-            img: event.target.result,
-          },
-        ]);
+        const imageProcessed = {
+          id: event.timeStamp,
+          bas64: event.target.result,
+          upload_status: "pending",
+        };
+
+        // once transformed, we store the image to render it on the page + proceed to sending it to Cloudinary
+        setImagesConverted((current) => [...current, imageProcessed]);
+        uploadImage(imageProcessed);
       };
 
       reader.readAsDataURL(file);
     });
   };
 
+  const uploadImage = async (imageProcessed) => {
+    const cloudinaryUrl = "https://api.cloudinary.com/v1_1/angelbt/auto/upload";
+
+    const formData = new FormData();
+    formData.append("file", imageProcessed.bas64);
+    formData.append("upload_preset", "gqqtbphk");
+    formData.append("folder", "cloudinary_example");
+
+    const cloudinaryOptions = { method: "POST", body: formData };
+
+    const response = await fetch(cloudinaryUrl, cloudinaryOptions);
+
+    if (!response.ok) {
+      setImagesUploaded((current) => [
+        ...current,
+        { ...imageProcessed, upload_status: "failed" },
+      ]);
+    }
+
+    const json = await response.json();
+    setImagesUploaded((current) => [
+      ...current,
+      { ...imageProcessed, upload_status: "success", url: json.secure_url },
+    ]);
+  };
+
   const removePhoto = (id) => {
-    setPhotosToUpload((current) => current.filter((file) => file.id !== id));
+    // we remove the image from both state variables
+    setImagesConverted((current) => current.filter((file) => file.id !== id));
+    setImagesUploaded((current) => current.filter((file) => file.id !== id)); // TODO send DELETE to Cloudinary
   };
 
   return (
     <>
       <div className="boxGrid">
-        {photosToUpload.length > 0 &&
-          photosToUpload.map((photoSelected) => {
+        {/* TODO add trash icon and upload status */}
+        {imagesConverted.length > 0 &&
+          imagesConverted.map((imageConverted) => {
+            {
+              /* TODO fix layout */
+            }
             return (
-              <div
-                className="photoBox"
-                onClick={() => removePhoto(photoSelected.id)}
-              >
-                <img
-                  alt=""
-                  key={photoSelected.id}
-                  src={photoSelected.img}
-                  className="img"
-                />
+              <div className="unflex">
+                <div
+                  className="photoBox"
+                  onClick={() => removePhoto(imageConverted.id)}
+                >
+                  <img
+                    alt=""
+                    key={imageConverted.id}
+                    src={imageConverted.bas64}
+                    className="img"
+                  />
+                </div>
+                <div>
+                  <p>
+                    {imagesUploaded.find((img) => img.id === imageConverted.id)
+                      ? imagesUploaded.find(
+                          (img) => img.id === imageConverted.id
+                        ).upload_status
+                      : imageConverted.upload_status}
+                  </p>
+                </div>
               </div>
             );
           })}
-        {[...Array(maxPhotos - photosToUpload.length)].map(() => {
+
+        {[...Array(maxImages - imagesConverted.length)].map(() => {
           return (
             <div className="box" onClick={openInput}>
               <div className="addSymbol">+</div>
@@ -73,10 +128,11 @@ const UploadImageInput = ({ maxPhotos = 1 }) => {
       <input
         type="file"
         className="hidden"
-        multiple={maxPhotos > 1}
+        multiple={maxImages > 1}
         accept="image/*"
         ref={inputRef}
         onChange={handleChange}
+        name={name}
       />
     </>
   );
